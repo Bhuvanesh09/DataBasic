80af386c2c535f6832f52293245a699fa11673c2
MG1800 <monilgokani08@gmail.com>
2019-11-12 22:35:22 +0530

    added report queries

--- setup.sql
+++ setup.sql
@@ -2,7 +2,7 @@ create database CALENDAR;
 use CALENDAR; 
 create table student (
     user_id varchar(10) unique not null,
-    user_name varchar(10) not null,
+    user_name varchar(30) not null,
     DOB DATE not null,
     gender varchar(6) not null,
     hostel varchar(30) not null,
@@ -13,7 +13,7 @@ create table student (
 
 create table administrator (
     user_id varchar(10) unique not null,
-    user_name varchar(10) not null,
+    user_name varchar(30) not null,
     DOB DATE not null,
     gender varchar(6) not null,
     designation varchar(20),
@@ -79,12 +79,11 @@ create table group_members(
     FOREIGN KEY (group_id) references user_group(group_id) 
 );
 
+
 insert into student values(21130, 'Ankit Roy', '1998-10-13', 'Male', 'Bakul', 2019, 'CND');
 insert into student values(21140, 'Ankita Ray', '1998-10-21', 'Female', 'Parijaat', 2018, 'ECE');
 
 insert into administrator values(51120,'Mahant Parekh', '1992-01-22', 'Male', 'Senior Administrator');
-alter table administrator user_name user_name varchar(30);
-alter table student user_name user_name varchar(30);
 insert into user_group values ('C101', 'Music', 'Club');
 insert into user_group values ('O121', 'NLPStudents', NULL);
 insert into user_group values ('D110', 'Mathematics', 'Department');
@@ -104,3 +103,39 @@ insert into group_members values (21140, 'C101');
 insert into club_co values ('C101', 21140);
 
 insert into event values('Guitar Workshop', 'Nilgiri', '105', 'C101', '13:00:00', '16:00:00', '2019-12-21');
+insert into event values('Tabla Workshop', 'Nilgiri', '105', 'C101', '13:00:00', '16:00:00', '2019-12-30');
+insert into event values('Language Inference', 'Himalaya', '203', 'O121', '13:00:00', '16:00:00', '2019-12-30');
+
+
+-- Report Generation queries
+
+-- 1> Events on a given date:
+
+-- select * from event where date = 'YYYY-MM-DD'
+
+-- 2> check if location is free during time slot
+-- select * from event 
+-- where building_name='location-building' and room_number='location-room' and event_date = 'time slot date' and 
+-- ( 
+--  (start_time between 'time slot end' and 'time slot end' or end_time between 'time slot begin' and 'time slot end')
+--  OR
+--  (start_time < 'time slot start' and end_time > 'time slot end' )
+-- );
+
+
+-- if this returns empty set, then time slot is free, otherwise not free.
+
+-- 3> check if a particular user group (provided group name) is invited to a particular event (provided event name)
+-- select name,event_name from user_group 
+-- join event on user_group.group_id = event.group_id 
+-- where event_name = 'given event name' and user_group.name = 'given group name';
+
+
+-- if this returns empty, not invited, if it returns non null, then yes, group is invited.
+
+
+-- 4>show all of a user's events arranged according to time
+-- select event_name, event_date, event_ from event 
+-- join group_members on group_members.group_id = event.group_id 
+-- where group_members.user_id = given user id
+-- order by event_date;
